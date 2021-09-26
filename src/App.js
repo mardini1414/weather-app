7965dc72daa23dd8dd83d587d85cab21ccfe4930
mardini-kompronk <mardini1414@gmail.com>
2021-09-26 15:09:58 +0700

    remove console

--- src/App.js
+++ src/App.js
@@ -16,17 +16,13 @@ function App() {
   const [temp, setTemp] = useState('');
 
   const getData = async () => {
-    try {
-      const resData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=cacb232f4b748d0760b2ba579000d0d8&units=metric
-      `);
-      setName(resData.data.name);
-      setCountry(resData.data.sys.country);
-      setImg(resData.data.weather[0].icon);
-      setWeather(resData.data.weather[0].description);
-      setTemp(resData.data.main.temp);
-    } catch (e) {
-      console.log(e);
-    }
+    const resData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=cacb232f4b748d0760b2ba579000d0d8&units=metric
+    `);
+    setName(resData.data.name);
+    setCountry(resData.data.sys.country);
+    setImg(resData.data.weather[0].icon);
+    setWeather(resData.data.weather[0].description);
+    setTemp(resData.data.main.temp);
     setInput('');
   };
 
