c01f463bf1f7d2777a49d0e32d29b4fa846b221e
mardini-kompronk <mardini1414@gmail.com>
2021-12-27 18:52:41 +0700

    add purge css

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,14 @@ import wave from '../assets/wave.svg';
 /* ------------------------------------------------------------------------ */
 
 function Main({
-  img, temp, name, country, weather, wind, humidity, visibility,
+  img,
+  temp,
+  name,
+  country,
+  weather,
+  wind,
+  humidity,
+  visibility,
 }) {
   return (
     <Row className="justify-content-center mt-2 px-1">
@@ -15,10 +22,20 @@ function Main({
             <i className="fas fa-map-marker-alt" />
             {` ${name}, ${country}`}
           </span>
-          <img className="my-3" src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="weather-img" width="150" />
+          <img
+            className="my-3"
+            src={`http://openweathermap.org/img/wn/${img}@2x.png`}
+            alt="weather-img"
+            width="150"
+          />
           <span className="temp my-3">{`${Math.round(temp)}°C`}</span>
           <span className="position-absolute bottom-0 mb-2">{weather}</span>
-          <img id="wave" src={wave} alt="wave" className="position-absolute bottom-0" />
+          <img
+            id="wave"
+            src={wave}
+            alt="wave"
+            className="position-absolute bottom-0"
+          />
         </div>
         <div className="mt-3 p-2 bg-light text-light rounded d-flex justify-content-around text-center">
           <div className="p-3">
